3ab45b55381e8b7bb6ae14bc449243cd808c2839
bala160 <krishjo160@gmail.com>
2024-09-01 13:47:51 +0000

    Changes done in multiple file

--- swagger/src/main/java/com/example/swagger/jumpStatement/breakStatement.java
+++ swagger/src/main/java/com/example/swagger/jumpStatement/breakStatement.java
@@ -2,7 +2,7 @@ package com.example.swagger.jumpStatement;
 
 public class breakStatement {
     public static void main(String[] args) {
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i < 9; i++) {
             if (i == 6) {
                 break;
             }
